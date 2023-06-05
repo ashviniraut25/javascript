3b8c731b2b25334d6e3d3688a13c6f9fd8183069
ashviniraut25 <ashviniraut25@gmail.com>
2023-06-05 19:00:46 +0530

    array

---
+++ 10_arrayduplicates.js
@@ -0,0 +1,24 @@
+const arrayNum =[11,3,4,11,4,7,3];
+console.log(`Given Array : ${arrayNum}`);
+const unique = [ ];
+const a = [ ];
+const b=[ ];
+for (let i = 0; i < arrayNum.length; i++) {
+    const element = arrayNum(i);
+    console.log(element);
+    
+
+    for (let j = 0; j < arrayNum.length; j++) {
+        const element1 = b[j];
+        if (element!=element1) {
+            unique.push[element1];
+            console.log(unique);
+            
+        }
+        
+    }
+}
+
+
+
+
